7a2020c3e6cd774c059c7f991d59c42d91b3ef2e
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2020-03-08 15:50:58 +0900

    updated

--- Q3.js
+++ Q3.js
@@ -1,127 +1,215 @@
+/*
+コメントボックスは目立ちすぎないように青を基調として設定しました。
+また、セレクトボックスやもテキストボックスにもマウスhover時のシャドーを
+加えました。
+*/
 
-          class CommentBox {
-            constructor(msg) {
-              // selectboxの近くに吹き出しを配置
-              this.$box = document.createElement('div')
-              this.$div1 = document.createElement('div')
-              this.$div2 = document.createElement('div')
-              this.$box.innerHTML = msg
-              // this.$div1.innerHTML = ' '
 
-              this.transform()
-              //ノードを返り値として設定
 
-              this.$box.appendChild(this.$div2)
-              this.$box.appendChild(this.$div1)
-              this.fragment = document.createDocumentFragment()
-              this.fragment.appendChild(this.$box)
-              // this.fragment.appendChild(this.$div)
-              this.addEvents()
+  class AddBalloons {
+      constructor() {
 
-              return this.fragment
-            }  
+        this.$commentBox1 = document.createElement('div')
+        this.$commentBox2 = document.createElement('div')
+        this.$selectBoxes = document.getElementsByTagName('select')
+        this.$textBox = document.getElementById('electricityUsage')
 
-            transform() {
-              const obj = {
-                box: {
-                  width: '100%',
-                  height: '60px',
-                  fontSize: '12px',
-                  position: 'relative',
-                  // backgroundColor: 'rgba(191,156,70,0.8)',
-                  backgroundColor: 'rgba(255,240,177,1)',
-                  color: 'rgb(0,0,0)',
-                  padding: '7px 15px 5px',
-                  marginBottom: '30px',
-                  borderRadius: '10px',
-                  border: '2px solid #000'
-                },
-                div: {
-                  position: 'absolute',
-                  bottom: '-20px',
-                  left: '5%',
-                  zIndex: '1',
-                  width: '0',
-                  height: '0',
-                  border: '10px solid transparent',
-                  // borderTopColor: 'rgba(191,156,70,0.8)'
-                  borderTopColor: 'rgba(255,240,177,1)'
-                }
-              }
+        //コメントボックスの仮想DOMを生成
+        this.createCommentBoxes()
+        //生成した仮想DOMをツリーに挿入
+        this.insertCommentBoxes()
 
-              for(let key in obj.box) {
-                this.$box.style[key] = obj.box[key];
-              }
+        //セレクトボックス・テキストボックスにイベント(hover/change)を付与
+        this.addEventsToFormItems()
+        //コメントボックスへのhoverイベント付与
+        this.addEventsToCommentBoxes()
 
-              for(let key in obj.div) {
-                this.$div1.style[key] = obj.div[key];
-                this.$div2.style[key] = obj.div[key];
-              }
+      }
+      
+      createCommentBoxes() {
 
-              this.$div2.style.bottom = '-22px'
-              this.$div2.style.borderTopColor = 'rgba(0,0,0,1)'
+        const messages = [
+            '　一般的には<b>従量電灯B</b>（関西・中国・四国の場合は<u>従量電灯A相当</u>）をご利用されている方が多いです',
+            '　一般的には<b>40A</b>をご利用されている方が多いです'
+        ]
 
-              
+        const commentStyles = {
+          parent: {
+            position: 'relative',
+            height: '70px',
+          },
+          box: {
+            width: '100%',
+            height: '60px',
+            fontSize: '12px',
+            position: 'absolute',
+            backgroundColor: '#E0E3DA',
+            // backgroundColor: 'rgba(255,240,177,1)',
+            color: 'rgb(0,0,0)',
+            padding: '7px 15px 5px',
+            marginBottom: '30px',
+            borderRadius: '10px',
+            border: '2px solid #9DC8C8'
+          },
+          triangle: {
+            front: {
+              position: 'absolute',
+              left: '5%',
+              zIndex: '1',
+              width: '0',
+              height: '0',
+              border: '10px solid transparent',
+              bottom: '-6px',
+              borderTopColor: '#E0E3DA'
+              // borderTopColor: 'rgba(255,240,177,1)'
+            },
+            back: {
+              position: 'absolute',
+              left: '5%',
+              zIndex: '1',
+              width: '0',
+              height: '0',
+              border: '10px solid transparent',
+              bottom: '-8px',
+              borderTopColor: '#9DC8C8'    
             }
-
-
-            addEvents() {
-              this.$box.addEventListener('mouseover', function(e) {
-
-              })
-
-            }
-
           }
+        }
 
-          msg1 = '　一般的には<b>従量電灯B</b>（関西・中国・四国の場合は<u>従量電灯A相当</u>）をご利用されている方が多いです'
-          msg2 = '　一般的には<b>40A</b>をご利用されている方が多いです'
+        const fragments = messages.map(function(message) {
+          /*
+          #fragmentの作成
+          |  <div>message</div>
+          |  <div></div>
+          |  <div></div>
+          */
+        const fragment = document.createDocumentFragment()
+        for(var i=0; i<3; i++) {
+          const div = document.createElement('div')
+          if(i === 0) {
+            div.insertAdjacentHTML('afterbegin', message)
+          }
+          fragment.appendChild(div)
+        }
 
+          /*
+           style設定
+          */
 
-          const comment1 = new CommentBox(msg1)
-          const comment2 = new CommentBox(msg2)
+        for(let key in commentStyles.box) {
+          fragment.children[0].style[key] = commentStyles.box[key]
+        }
 
-          const cont_area = document.getElementsByClassName('cont_area')
-
-          const h3_1 = cont_area[1].getElementsByTagName('h3')[0]
-          cont_area[1].insertBefore(comment1, h3_1.nextSibling)
-
-
-          const h3_2 = cont_area[2].getElementsByTagName('h3')[0]
-          cont_area[2].insertBefore(comment2, h3_2.nextSibling)
-
+        for(let key in commentStyles.triangle.back) {
+          fragment.children[1].style[key] = commentStyles.triangle.back[key]
+        }
         
-  //hoverしたときに光らせたい・カーソル変えたい
-  const select = document.getElementsByTagName('select')
+        for(let key in commentStyles.triangle.front) {
+          fragment.children[2].style[key] = commentStyles.triangle.front[key]
+        }
 
-  select[0].addEventListener('change', function(e) {
-    if(e.target.value !== '') {
+          return fragment
+        })
+
+        const self = this
+        for(let key in commentStyles.parent) {
+          self.$commentBox1.style[key] = commentStyles.parent[key]
+          self.$commentBox2.style[key] = commentStyles.parent[key]
+        }
+
+        //あとで削除するためにクラス名を付与
+        this.$commentBox1.classList.add('comment-box')
+        this.$commentBox2.classList.add('comment-box')
+
+        this.$commentBox1.appendChild(fragments[0])
+        this.$commentBox2.appendChild(fragments[1])
+
+      }
+
+
+      insertCommentBoxes() {
+
+        // プラン
+        const planParent = this.$selectBoxes[1].parentNode.parentNode
+        planParent.insertBefore(this.$commentBox1, planParent.children[0])
+
+        // アンペア
+        const ampParent = this.$selectBoxes[2].parentNode.parentNode
+        ampParent.insertBefore(this.$commentBox2, ampParent.children[0])
+
+      }
+
+      addEventsToFormItems() {
+
+        const shadowStyles = {
+          add: {
+            boxShadow: '0px 0px 5px 5px #9DC8C8',
+            cursor: 'pointer',
+            transition: '0.1s',  
+          },
+          remove: {
+            boxShadow: '',
+            cursor: '',
+            transition: '0.1s'
+          }
+        }
+
+        //mouseover
+        function addShadow(e) {
+          for(let key in shadowStyles.add) {
+            e.currentTarget.style[key] = shadowStyles.add[key]  
+          }
+        }
+
+        //mouseout
+        function removeShadow(e) {
+          for(let key in shadowStyles.remove) {
+            e.currentTarget.style[key] = shadowStyles.remove[key]  
+          }
+        }
+
+        //change
+        function removeComment(e) {
+          const targetCommentBox = e.currentTarget.parentNode.parentNode.children[0]
+          if(targetCommentBox.classList.contains('comment-box')) {
+            targetCommentBox.style.display = 'none'
+          }
+        }
+
+        Array.from(this.$selectBoxes).forEach(function($selectBox) {
+          $selectBox.addEventListener('mouseover', addShadow)
+          $selectBox.addEventListener('mouseout', removeShadow)
+          $selectBox.addEventListener('change', removeComment)
+        })
+
+        this.$textBox.addEventListener('focus', addShadow)
+        this.$textBox.addEventListener('blur', removeShadow)
+
+      }
+
+      addEventsToCommentBoxes() {
+
+        //mouseover
+        function zoomUpComment(e) {
+          e.currentTarget.style.transition = '0.1s'
+          e.currentTarget.style.transform = 'scale(1.05, 1.05)'
+        }
+
+        //mouseout
+        function zoomOutComment(e) {
+          e.currentTarget.style.transition = '0.1s'
+          e.currentTarget.style.transform = 'scale(1, 1)'
+        }
+
+        this.$commentBox1.addEventListener('mouseover', zoomUpComment)
+        this.$commentBox1.addEventListener('mouseout',  zoomOutComment)
+
+        this.$commentBox2.addEventListener('mouseover', zoomUpComment)
+        this.$commentBox2.addEventListener('mouseout',  zoomOutComment)
+
+      }
+
 
     }
-  })
 
-  Array.from(select).forEach(function(element) {
-    element.addEventListener('mouseover', function(e) {
-      this.style.boxShadow = '0px 0px 5px 5px rgba(66,103,178,0.7)'
-      this.style.cursor = 'pointer'
-      this.style.transition = '1s'
-    })
-    element.addEventListener('mouseout', function(e) {
-      this.style.boxShadow = ''
-      this.style.cursor = ''
-      this.style.transition = '1s'
-    })
-  })
-
-//fragmentでなくdivで囲むようにする
-//hoverしたら拡大する
-//selectboxをchangeで消せるようにする
-//やはりclassを一つにまとめる
-//二番目のコメントの位置おかしい
-
-
-
-
-
-
-
+    new AddBalloons()

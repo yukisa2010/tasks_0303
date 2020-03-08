b24035f428eb2a57266e155d17f1301bb93d7f5f
yukisa2010 <43121712+yukisa2010@users.noreply.github.com>
2020-03-08 19:23:51 +0900

    updated

--- Q1.js
+++ Q1.js
@@ -17,29 +17,37 @@ class FloatBox {
     }
 
     for(let key in obj) {
-      this.$cloneNode.style['key'] = obj[key];
+      this.$cloneNode.style[key] = obj[key];
     }
     
-    this.$contBlock.parentElement.appendChild(cloneNode);
+    this.$contBlock.parentElement.appendChild(this.$cloneNode);
   
   }
 
   addEvents() {
 
+    const pos = {
+      start: 1600,
+      end: 1758 
+    }
+
+    const self = this
     window.addEventListener('scroll', function() {
-      if(this.scrollY >= 1600 && this.scrollY <= 1758) {
-        cloneNode.style.opacity = (1758 - this.scrollY)/158;
-      } else if(this.scrollY > 1758) {
-        cloneNode.style.opacity = 0;
-      } else if(this.scrollY < 1600) {
-        cloneNode.style.opacity = 1;
+      if(this.scrollY >= pos.start && this.scrollY <= pos.end) {
+        self.$cloneNode.style.opacity = (pos.end - this.scrollY)/158;
+        self.$cloneNode.style.display = 'block'
+      } else if(this.scrollY > pos.end) {  
+        self.$cloneNode.style.opacity = 0;
+        self.$cloneNode.style.display = 'none'
+      } else if(this.scrollY < pos.start) {
+        self.$cloneNode.style.opacity = 1;
+        self.$cloneNode.style.display = 'block'
       }      
     })
   
   }
 }
 
-//消去位置の設定も外部から行いたい。
 
 new FloatBox();
 
